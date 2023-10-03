f5a4c6720418e29f0b4d6f93bffd42f38f6b8398
FatemeAmlahi <101039026+FatemeAmlahi@users.noreply.github.com>
2023-10-03 04:00:36 -0700

    finished portfolio swiper

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function opentab(name) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(name).classList.add("active-content");
 }
+
+let swiperCards = new Swiper(".card-content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCurser: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 3,
+    },
+  },
+});
